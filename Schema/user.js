629cd4c5cac7ecf1a41f02177387a44a95b4a82c
NathanUTD2140 <ngb210000@utdallas.edu>
2026-06-23 17:07:03 -0500

    User schema
    Added in a user schema with a basic password, username, and high score so it can keep track of a streak. Added in loadDatabase.js to test if the schemas could be made and created on the mongoDB atlas database with the connection.

---
+++ Schema/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  // Name of the file containing the photo (images directory).
+  user_name: {
+    type: String, //username basically
+    required: true, //everyone needs to have a name
+    unique: true, //no repeating names
+    trim: true //trim excess spacess
+  },
+  high_score: { 
+    type: [Number], 
+    default : [] 
+    }, 
+  password_digest:{
+    type: String, //string for the user 
+    required: true, //needs to be there
+  }
+});
+
+/**
+ * Create a Mongoose Model for a Photo using the photoSchema.
+ */
+const User = mongoose.model("User", userSchema);
+
+/**
+ * Make this available to our application.
+ */
+export default User;
